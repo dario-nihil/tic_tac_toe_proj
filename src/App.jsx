5303adc822883685bb71fd06014088d2589e9405
dario-nihil <dario.nihil@gmail.com>
2024-05-13 15:08:57 +0200

    get rid of currentPlayer state, and derive info from gameTurns state

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,27 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/PLayer";
 import Log from "./components/Log";
 
+const deriveActivePlayer = (gameTurns) => {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+};
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  // const [activePlayer, setActivePlayer] = useState("X");
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   const handleSelectSquare = (rowIdx, colIdx) => {
-    setActivePlayer((prevActivePlayer) =>
-      prevActivePlayer === "X" ? "O" : "X"
-    );
+    // setActivePlayer((prevActivePlayer) =>
+    //   prevActivePlayer === "X" ? "O" : "X"
+    // );
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowIdx, col: colIdx }, player: currentPlayer },
